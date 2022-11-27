f06d5509acbb6093b76e4c20c2eeaf99482dd67e
zEEmperon <dan4ik2204.danya@gmail.com>
2022-11-27 21:16:04 +0200

    classification method

--- main.py
+++ main.py
@@ -102,6 +102,45 @@ def get_R_and_phi(df: pd.DataFrame) -> list:
     return res_arr
 
 
+def classify(df: pd.DataFrame, threshold_phi: int, phi_list: list) -> list:
+    get_class = lambda y: 'K1' if y == 1 else 'K2'
+
+    n_K1 = len(get_K1(df))
+    n_K2 = len(get_K2(df))
+
+    res = []
+    for i in df.index:
+        actual_class = get_class(df.loc[i]['Class'])
+        neighborhoods = [*filter(lambda x: x[0] == i or x[1] == i, phi_list)]
+
+        k1_phi = 0
+        k2_phi = 0
+        a = 1
+
+        for n in neighborhoods:
+            n_index = n[0] if n[0] != i else n[1]
+            n_phi = n[2]
+            if df.loc[n_index]['Class'] == 1:
+                k1_phi += n_phi
+            else:
+                k2_phi += n_phi
+
+        total_potential = (1 / n_K1) * k1_phi + (1 / (n_K2 - 1)) * k2_phi
+        predicted_class = 'K1' if total_potential >= threshold_phi else 'K2'
+
+        res.append([i, total_potential, predicted_class, actual_class])
+
+    return res
+
+
+def get_stats(classified: list) -> list:
+    return []
+
+
+def print_stats(classified: list) -> None:
+    pass
+
+
 def main():
     training_set_filename = "training_set.csv"
     test_set_filename = "test_set.csv"
@@ -130,6 +169,16 @@ def main():
     print()
     print_task("3.5 і 3.6")
     print(label)
+    print(tabulate(table_data, headers=col_names, tablefmt="fancy_grid"))
+
+    # classify
+    threshold_phi = 34
+    phi_for_training_set = [*map(lambda i: [i[0], i[1], i[2]], R_and_phi_for_training_set)]
+
+    col_names = ['Номер примірника', 'Сумарний потенціал', 'Прогн. клас', 'Справжній клас']
+    table_data = classify(training_df, threshold_phi, phi_for_training_set)
+
+    print()
     print(tabulate(table_data, headers=col_names, tablefmt="fancy_grid"))
 
 

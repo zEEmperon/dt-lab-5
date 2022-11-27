fe1936dda5cefd1c68881dc4676fdc3498f99611
zEEmperon <dan4ik2204.danya@gmail.com>
2022-11-27 16:45:16 +0200

    task 3.5

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import math
+from typing import Tuple, Any
+
 import pandas as pd
 import numpy as np
 from tabulate import tabulate
+from itertools import combinations
 
 
 def load_csv(filename):
@@ -51,6 +55,10 @@ def get_normed_x(df: pd.DataFrame, d_x: dict) -> pd.DataFrame:
     return df / d_x.values()
 
 
+def get_instances_combinations(list_with_indexes: list) -> list:
+    return [*combinations(list_with_indexes, 2)]
+
+
 def get_beta_coefs(df: pd.DataFrame) -> np.array:
     df_K1 = get_K1(df)
     df_K2 = get_K2(df)
@@ -75,6 +83,15 @@ def get_beta_coefs(df: pd.DataFrame) -> np.array:
     return beta_coefs
 
 
+def get_generalized_R(df: pd.DataFrame) -> list:
+    instances_combinations = get_instances_combinations(df.index)
+    res = []
+    for comb in instances_combinations:
+        general_R = math.sqrt(sum((df.loc[comb[0]] - df.loc[comb[1]]) ** 2))
+        res.append([comb[0], comb[1], general_R])
+    return res
+
+
 def main():
     training_set_filename = "training_set.csv"
     test_set_filename = "test_set.csv"
@@ -91,6 +108,19 @@ def main():
     m_x_for_training_set = get_M_x_for_all_x(training_df_without_class_attr)
     d_x_for_training_set = get_D_x_for_all_x(training_df_without_class_attr, m_x_for_training_set)
     normed_x_training_set = get_normed_x(training_df_without_class_attr, d_x_for_training_set)
+
+    # Узагальнена відстань для і-ого і j-ого екземплярів
+    # 276 для навчальної вибірки
+    label = "Узагальнена відстань для і-ого і j-ого екземплярів"
+    generalized_R_for_training_set = get_generalized_R(training_df_without_class_attr)
+
+    table_data = [*map(lambda results: [results[0] + 1, results[1] + 1, results[2]], generalized_R_for_training_set)]
+    col_names = ["Номер i-го примірника", "Номер j-го примірника", "Узагальнена відстань"]
+
+    print()
+    print_task(3.5)
+    print(label)
+    print(tabulate(table_data, headers=col_names, tablefmt="fancy_grid"))
 
 
 if __name__ == '__main__':
